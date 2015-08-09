17bfdf2f6a742da187341772da15ee69fc19dace
teoadie <teoadie@mail.com>
2015-08-09 16:58:24 +0300

    Fixteres for pytests are added

--- add_contact_test.py
+++ add_contact_test.py
@@ -1,100 +1,54 @@
 # -*- coding: utf-8 -*-
-import unittest
-
-from selenium.webdriver.firefox.webdriver import WebDriver
-from pages import main_page
+import pytest
+from fixtures.contact_fixture import ContactFixture
 from model.contact_data import Contact
-from pages import new_contact_page
+
+@pytest.fixture
+def app(request):
+    fixture = ContactFixture()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+def test_add_contact(app):
+    # Create new contact object and fill its data
+    contact = Contact(firstname="Ann", middlename="AT", lastname="Terner", nickname="Tee")
+    contact.set_company_data(title="MegaMailGroutOfAllWorld", company="MegaMailGroupOfAllWorldCorporation",
+                             address="somewhere beyond the sea")
+    contact.set_emails(first="ann.terner@megamailgroupofallworldcorporation.com",
+                       second="ann@annhomemail.org", third="annnew@annhomemail.org")
+    contact.set_homepage("www.somewherebeyondthesea.com")
+    contact.set_phones(home="112223366", mobile="+7(000)9999911111222", work="44444433", fax="2300011")
+    contact.set_second_info(address="World 36", home="77", notes="She is my friend")
+    contact.set_birthday(day="4", month="4", year="1987")
+    contact.set_anniversary(day="31", month="7", year="2010")
+    # Create new contact
+    app.login()
+    app.open_contact_page_and_create_new_contact(contact)
+    # logout
+    app.logout()
 
 
-class add_contact_test(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
-    
-    def test_add_contact(self):
-        wd = self.wd
-        main_page.open_home_page(wd)
-        main_page.login_as_admin(wd)
-        # Create new contact and fill its data
-        contact = Contact(firstname="Ann", middlename="AT", lastname="Terner", nickname="Tee")
-        contact.set_company_data(title="MegaMailGroutOfAllWorld", company="MegaMailGroupOfAllWorldCorporation",
-                                 address="somewhere beyond the sea")
-        contact.set_emails(first="ann.terner@megamailgroupofallworldcorporation.com",
-                           second="ann@annhomemail.org", third="annnew@annhomemail.org")
-        contact.set_homepage("www.somewherebeyondthesea.com")
-        contact.set_phones(home="112223366", mobile="+7(000)9999911111222", work="44444433", fax="2300011")
-        contact.set_second_info(address="World 36", home="77", notes="She is my friend")
-        contact.set_birthday(day="4", month="4", year="1987")
-        contact.set_anniversary(day="31", month="7", year="2010")
-        # Create new contact
-        self.add_new_contact(wd)
-        # Fill form with data
-        new_contact_page.fill_primary_contact_data(wd, contact)
-        new_contact_page.fill_secondary_contact_data(wd, contact)
-        new_contact_page.fill_birthday_data(wd, contact)
-        new_contact_page.fill_anniversary_data(wd, contact)
-        new_contact_page.confirm_contact_creation(wd)
-        # Check contact
-        self.return_to_home_page(wd)
-        main_page.logout(wd)
-
-    def test_add_empty_contact(self):
-        wd = self.wd
-        main_page.open_home_page(wd)
-        main_page.login_as_admin(wd)
-        # Create new contact
-        self.add_new_contact(wd)
-        # Clear form
-        new_contact_page.clear_new_contact_form(wd)
-        # Confirm contract data
-        new_contact_page.confirm_contact_creation(wd)
-        # Check contact
-        self.return_to_home_page(wd)
-        main_page.logout(wd)
-
-    def test_add_contact_with_spaces_in_fields(self):
-        wd = self.wd
-        main_page.open_home_page(wd)
-        main_page.login_as_admin(wd)
-        # Create new contact and fill its data
-        contact = Contact(firstname=" ", middlename=" ", lastname=" ", nickname=" ")
-        contact.set_company_data(title=" ", company=" ", address=" ")
-        contact.set_emails(first=" ", second=" ", third=" ")
-        contact.set_homepage(" ")
-        contact.set_phones(home=" ", mobile=" ", work=" ", fax=" ")
-        contact.set_second_info(address=" ", home=" ", notes=" ")
-        contact.set_birthday(day="1", month="1", year=" ")
-        contact.set_anniversary(day="1", month="1", year=" ")
-        # Create new contact
-        self.add_new_contact(wd)
-        # Fill form with data
-        new_contact_page.fill_primary_contact_data(wd, contact)
-        new_contact_page.fill_secondary_contact_data(wd, contact)
-        new_contact_page.fill_birthday_data(wd, contact)
-        new_contact_page.fill_anniversary_data(wd, contact)
-        new_contact_page.confirm_contact_creation(wd)
-        # Check contact
-        self.return_to_home_page(wd)
-        main_page.logout(wd)
-
-    def return_to_home_page(self, wd):
-        wd.find_element_by_link_text("home page").click()
-
-    def add_new_contact(self, wd):
-        wd.find_element_by_link_text("add new").click()
-
-    def tearDown(self):
-        self.wd.quit()
+def test_add_empty_contact(app):
+    # Create new contact
+    app.login()
+    app.open_contact_page_and_create_new_contact(None)
+    # logout
+    app.logout()
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_add_contact_with_spaces_in_fields(app):
+    # Create new contact object and fill its data
+    contact = Contact(firstname=" ", middlename=" ", lastname=" ", nickname=" ")
+    contact.set_company_data(title=" ", company=" ", address=" ")
+    contact.set_emails(first=" ", second=" ", third=" ")
+    contact.set_homepage(" ")
+    contact.set_phones(home=" ", mobile=" ", work=" ", fax=" ")
+    contact.set_second_info(address=" ", home=" ", notes=" ")
+    contact.set_birthday(day="1", month="1", year=" ")
+    contact.set_anniversary(day="1", month="1", year=" ")
+    # Create new contact
+    app.login()
+    app.open_contact_page_and_create_new_contact(contact)
+    # logout
+    app.logout()
--- add_group_test.py
+++ add_group_test.py
@@ -1,78 +1,31 @@
 # -*- coding: utf-8 -*-
-import unittest
-
-from selenium.webdriver.firefox.webdriver import WebDriver
+import pytest
+from fixtures.group_fixture import GroupFixture
 from model.group_data import Group
-from pages import main_page
-from pages import new_group_page
 
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+@pytest.fixture
+def app(request):
+    fixture = GroupFixture()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-class add_group_test(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
-
-    def test_add_group(self):
-        wd = self.wd
-        main_page.open_home_page(wd)
-        main_page.login_as_admin(wd)
-        self.open_groups_page(wd)
-        group = Group(name='Best friends', header='My best friends', footer='Hell yeah')
-        self.create_group(wd, group)
-        self.return_to_groups_page(wd)
-        main_page.logout(wd)
-
-    def test_add_group_with_spaces(self):
-        wd = self.wd
-        main_page.open_home_page(wd)
-        main_page.login_as_admin(wd)
-        self.open_groups_page(wd)
-        group = Group(name=' ', header=' ', footer=' ')
-        self.create_group(wd, group)
-        self.return_to_groups_page(wd)
-        main_page.logout(wd)
-
-    def test_add_empty_group(self):
-        wd = self.wd
-        main_page.open_home_page(wd)
-        main_page.login_as_admin(wd)
-        self.open_groups_page(wd)
-        # Init group creation
-        wd.find_element_by_name('new').click()
-        # Clear group form
-        new_group_page.clear_new_group_form(wd)
-        # Confirm group creation
-        new_group_page.confirm_group_creation(wd)
-        self.return_to_groups_page(wd)
-        main_page.logout(wd)
-
-    def open_groups_page(self, wd):
-        # Open groups page
-        wd.find_element_by_link_text('groups').click()
-
-    def create_group(self, wd, group):
-        # Init group creation
-        wd.find_element_by_name('new').click()
-        # Fill group data
-        new_group_page.fill_group_page(wd, group)
-        # Confirm group creation
-        new_group_page.confirm_group_creation(wd)
-
-    def return_to_groups_page(self, wd):
-        # Return to group page
-        wd.find_element_by_link_text('group page').click()
-
-    def tearDown(self):
-        self.wd.quit()
+def test_add_group(app):
+    app.login()
+    group = Group(name='Best friends', header='My best friends', footer='Hell yeah')
+    app.open_group_page_and_create_new_group(group)
+    app.logout()
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_add_group_with_spaces(app):
+    app.login()
+    group = Group(name=' ', header=' ', footer=' ')
+    app.open_group_page_and_create_new_group(group)
+    app.logout()
+
+
+def test_add_empty_group(app):
+    app.login()
+    app.open_group_page_and_create_new_group(None)
+    app.logout()
